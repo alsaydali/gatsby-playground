ea79b1bf4d9019453ceee465b2273695f2dbe795
Marwan Alsaydali <alsaydali@icloud.com>
2020-04-07 13:03:07 -0400

    Gatsby config

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `Welcome to my Blog`,
+    siteUrl: `https://github.com/alsaydali/gatsby-playground`,
+    description: `I built this blog using GatsbyJs and GraphQL`,
+    author: {
+      name: `Marwan Alsaydali`,
+      summary: `I am a  Husband, Father, Webd Developer and Photographer.`,
+    },
+    social: {
+      twitter: `@alsaydali`,
+    },
+  },
+};
