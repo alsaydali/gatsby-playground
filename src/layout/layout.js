e7939c1a6a83fc6218b3669ad093a7bc98bd9873
Marwan Alsaydali <alsaydali@icloud.com>
2020-04-09 12:49:45 -0400

    refactore layout with graphql

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,12 +1,39 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
 import Nav from "../components/static/nav"
 import Footer from "../components/static/footer"
-const Layout = ({ children }) => (
-  <>
-    <Nav />
-    {children}
-    <Footer />
-  </>
-)
+const Layout = ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            author {
+              name
+              summary
+            }
+            description
+            title
+            social {
+              behance
+              github
+              instagram
+              twitter
+            }
+          }
+        }
+      }
+    `
+  )
+  const siteTitle = data.site.siteMetadata.title
+  return (
+    <>
+      <Nav sitetitle={siteTitle} />
+      {children}
+      <Footer sitetitle={siteTitle} />
+    </>
+  )
+}
 
 export default Layout
