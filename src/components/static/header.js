d3392838cc5f96ae92eb070b9c754c488ff018fb
Marwan Alsaydali <alsaydali@icloud.com>
2020-04-09 21:16:08 -0400

    some change in header

--- src/components/static/header.js
+++ src/components/static/header.js
@@ -3,6 +3,7 @@ import React from "react"
 const Header = (props) => (
   <header className="container hero">
     <h2>{props.title}</h2>
+    <p>{props.desc}</p>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,35 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../layout/layout"
 import Header from "../components/static/header"
-const indexPage = () => (
+const indexPage = ({ data }) => (
   <Layout>
-    <Header title="Home" />
+    <Header
+      title={data.site.siteMetadata.title}
+      desc={data.site.siteMetadata.description}
+    />
   </Layout>
 )
 
 export default indexPage
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        author {
+          name
+          summary
+        }
+        description
+        title
+        social {
+          behance
+          github
+          instagram
+          twitter
+        }
+      }
+    }
+  }
+`
