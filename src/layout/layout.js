058a17ac25d6c64f9fdd4821b4c151f3126e5469
Marwan Alsaydali <alsaydali@icloud.com>
2020-04-11 10:21:42 -0400

    some update in the layouts

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { Helmet } from "react-helmet"
 
 import Nav from "../components/static/nav"
 import Footer from "../components/static/footer"
@@ -29,6 +30,13 @@ const Layout = ({ children }) => {
   const siteTitle = data.site.siteMetadata.title
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{siteTitle}</title>
+        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      </Helmet>
       <Nav sitetitle={siteTitle} />
       {children}
       <Footer sitetitle={siteTitle} />
